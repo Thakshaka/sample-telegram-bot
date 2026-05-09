93e1467b90928e8838e28aab3c3b7aa9ac6dccbe
Thakshaka Rathnayake <76155498+Thakshaka@users.noreply.github.com>
2026-05-09 21:04:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Hi")
+    bot.reply_to(message, "Test")
 
 
 print("Bot is running...")
