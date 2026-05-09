dfd7e0ba64f739b5848569e3ed433d7fe97288d3
Thakshaka Rathnayake <76155498+Thakshaka@users.noreply.github.com>
2026-05-09 19:56:22 +0530

    Change greeting from 'Hello' to 'Heya'

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Hello")
+    bot.reply_to(message, "Heya")
 
 
 print("Bot is running...")
