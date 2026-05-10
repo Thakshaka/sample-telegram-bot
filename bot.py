4b459485d64338dbf8ab45d4864b37f0e6b23535
Thakshaka Rathnayake <76155498+Thakshaka@users.noreply.github.com>
2026-05-10 10:27:52 +0530

    Change bot reply message from 'Test 4' to 'Test 5'

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Test 4")
+    bot.reply_to(message, "Test 5")
 
 
 print("Bot is running...")
